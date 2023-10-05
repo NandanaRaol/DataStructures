343a22be3497a16d57fc7bd631e54d246b7bfc31
NandanaRaol <142094872+NandanaRaol@users.noreply.github.com>
2023-10-05 21:57:45 +0530

    Add files via upload

---
+++ colname.cpp
@@ -0,0 +1,58 @@
+/*
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+Excel columns has a pattern like A, B, C, … ,Z, AA, AB, AC,…. ,AZ, BA, BB, … ZZ, AAA, AAB …..
+ etc. In other words, column 1 is named as “A”, column 2 as “B”, column 27 as “AA” and so on.
+
+Example 1:
+
+Input:
+N = 28
+Output: AB
+Explanation: 1 to 26 are A to Z.
+Then, 27 is AA and 28 = AB.
+
+Example 2:
+
+Input: 
+N = 13
+Output: M
+Explanation: M is the 13th character of
+alphabet.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function colName()
+ which takes the column number N as input and returns the column name represented as a string.
+Expected Time Complexity: O(LogN).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= N <= 1018*/
+#include<bits/stdc++.h>
+using namespace std;
+ string colName (long long int n)
+    {
+      if(n<=26)
+      {
+          string sd="";
+          sd=sd+char(64+n);
+          return sd;
+      }
+      string ans="";
+      int t;
+      while(n>0)
+      {
+         n--;
+         t=n%26;
+         ans=ans+char(65+(t));
+         n=n/26;
+      }
+      reverse(ans.begin(),ans.end());
+      cout<<ans;
+      return ans;
+    }
+ int main()
+ {
+   int n;
+   cout<<"\n enter the number:";
+   cin>>n;
+   colName(n);	
+ }   
